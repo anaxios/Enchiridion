88a5b7fb4af89349b1addc2b7a7d2e0576b40af0
anaxios <anaxios@daedalist.net>
2024-07-31 07:45:20 -0700

    remove stale code

--- rename_batch.sh
+++ rename_batch.sh
@@ -3,25 +3,11 @@
 INPUT_FOLDER="output/Prologue_of_Ochrid"
 OUTPUT_FOLDER="output/prologue_name_change"
 
-spin() {
-    while true; do
-        spinner=( '-' '\' '|' '/' )
-        for i in "${spinner[@]}"; do
-            echo -ne "\r${i} $@"
-            sleep .05
-        done
-    done
-}
 
 i="1"
 for file in "$INPUT_FOLDER"/*; do
     filename="${file##*/}"
     filename="${filename:3}"
-
-    # if [ -e "$OUTPUT_FOLDER/$filename" ] && [ ! -s "$OUTPUT_FOLDER/$filename" ]; then
-    #     echo "Checking if empty: $filename"
-    #     rm "$OUTPUT_FOLDER/$filename"
-    # fi
 
     if [ -f "$file" ] && [ ! -e "$OUTPUT_FOLDER/$filename" ]; then
         # let paddedI=$(printf "%03d" $i)
